9a9072e1926623dfa9f691f0e8020a98b8807af1
Chase <chaseaohill@gmail.com>
2023-02-27 23:47:33 -0500

    Completed constants file for URL construction

--- Assessment-4-Wall-E/Assessment-4-Wall-E/SupportingFiles/Constants.swift
+++ Assessment-4-Wall-E/Assessment-4-Wall-E/SupportingFiles/Constants.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Constants {
+    
+    struct NASAURL {
+    //https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=6FWUge4KKZMAWtAY8KrBypHzHiPnQMitR3EhbTRX&earth_date=2015-6-3
+        static let baseURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+        static let queryKey = "api_key"
+        static let queryValue = "6FWUge4KKZMAWtAY8KrBypHzHiPnQMitR3EhbTRX"
+        static let dateKey = "earth_date"
+    }
+}
